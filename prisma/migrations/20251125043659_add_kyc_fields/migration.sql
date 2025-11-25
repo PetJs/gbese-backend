ce621046022ee8a24e0def091289fda2e5b7d056
PetJs <peterfagoroye76@gmail.com>
2025-11-25 05:55:33 +0100

    feat: Add KYC fields (address, city, country, date_of_birth, occupation, postal_code, state) to the user table.

---
+++ prisma/migrations/20251125043659_add_kyc_fields/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `address` VARCHAR(255) NULL,
+    ADD COLUMN `city` VARCHAR(100) NULL,
+    ADD COLUMN `country` VARCHAR(100) NULL,
+    ADD COLUMN `date_of_birth` DATE NULL,
+    ADD COLUMN `occupation` VARCHAR(100) NULL,
+    ADD COLUMN `postal_code` VARCHAR(20) NULL,
+    ADD COLUMN `state` VARCHAR(100) NULL;
